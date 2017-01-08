a428e0792cae9792168c0cadfbbfd9676f988b0c
ajbater <amanda22@hotmail.co.uk>
2017-01-08 14:51:16 +0000

    article list can create and store articles

--- spec/article-list-spec.js
+++ spec/article-list-spec.js
@@ -3,3 +3,17 @@ function testArticleListModelCanBeInstantiated() {
   assert.isTrue(articleList);
 };
 testArticleListModelCanBeInstantiated();
+
+function testArticleListCreatesANewArticle() {
+  articleList = new ArticleList();
+  articleList.createArticle("headline", "full article text");
+  assert.isTrue(articleList.articles[0] === article);
+};
+testArticleListCreatesANewArticle();
+
+function testArticleListModelStoresArticles() {
+  articleList = new ArticleList();
+  articleList.createArticle("headline", "full article text");
+  assert.isTrue(articleList.returnList().length === 1);
+};
+testArticleListModelStoresArticles();
--- src/article-list.js
+++ src/article-list.js
@@ -1,7 +1,16 @@
 (function(exports) {
 
   function ArticleList() {
+    this.articles = [];
+  };
 
+  ArticleList.prototype.createArticle = function(headline, fullText) {
+    article = new Article(headline, fullText);
+    this.articles.push(article);
+  };
+
+  ArticleList.prototype.returnList = function() {
+    return this.articles;
   };
 
   exports.ArticleList = ArticleList;
