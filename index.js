f2e74f43c8eb46af926c7309e89fba79fbe44750
ajbater <amanda22@hotmail.co.uk>
2017-01-08 16:34:32 +0000

    create index.js file

---
+++ index.js
@@ -0,0 +1,5 @@
+var articleList = new ArticleList();
+var controller = new Controller(articleList);
+controller.articleList.createArticle("headline", "full text");
+controller.articleList.createArticle("headline", "full text");
+controller.updateHtml();
