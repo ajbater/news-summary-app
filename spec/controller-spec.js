7419ea3fe5f92e1dc4e5d26c4b66cfd361366e58
ajbater <amanda22@hotmail.co.uk>
2017-01-08 16:36:07 +0000

    controller can create articles and update the html with the headlines as links

--- spec/controller-spec.js
+++ spec/controller-spec.js
@@ -11,3 +11,24 @@ function testControllerIsPassedAnArticleListAndUsesThisToCreateAnArticleListView
   assert.isTrue(controller.articleListView instanceof ArticleListView);
 };
 testControllerIsPassedAnArticleListAndUsesThisToCreateAnArticleListView();
+
+// function testCanUpdateHtml() {
+//   function ArticleListViewDouble() {
+//     this.getArticleListCallCount = 0;
+//   };
+//
+//   ArticleListViewDouble.prototype = {
+//     getArticleList: function() {
+//       this.returnHtmlListCallCount += 1;
+//       return "headline";
+//     }
+//   };
+//
+//   articleList = new ArticleList();
+//   controller = new Controller(articleList);
+//   double = new ArticleListViewDouble();
+//   console.log(controller);
+//   controller.updateHtml(double);
+//   assert.isTrue(double.returnHtmlListCallCount === 1);
+// };
+// testCanUpdateHtml();
--- src/controller.js
+++ src/controller.js
@@ -5,6 +5,16 @@
     this.articleListView = new ArticleListView(this.articleList);
   };
 
+  Controller.prototype.createArticle = function(headline, fullText) {
+    this.articleList.createArticle(headline, fullText);
+    return this.articleList.returnList();
+  };
+
+  Controller.prototype.updateHtml = function(articleListView) {
+    var element = document.getElementById("app");
+    element.innerHTML = this.articleListView.returnHtmlList();
+  };
+
   exports.Controller = Controller;
 
 })(this);
