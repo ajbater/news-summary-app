9761733e7aaa065b62ae965677800674170cf155
ajbater <amanda22@hotmail.co.uk>
2017-01-08 14:31:09 +0000

    guardian api request model can make a request url to get headlines for todays date

--- src/guardian-api-request.js
+++ src/guardian-api-request.js
@@ -2,8 +2,13 @@
 
   function GuardianApiRequest() {
     this.makersApi = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=";
+    this.articlesUrl = "http://content.guardianapis.com/search?from-date=";
     this.date = new Date();
-    this.dateString = this.date.getMonth() + 1 + "-" + this.date.getDate() + "-" + this.date.getFullYear();
+    this.dateString = this.date.toISOString().split('T')[0];
+  };
+
+  GuardianApiRequest.prototype.createRequestUrl = function() {
+    return this.makersApi + this.articlesUrl + this.dateString;
   };
 
   exports.GuardianApiRequest = GuardianApiRequest;
