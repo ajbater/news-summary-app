0bc9e0cffb3229f55ea18fc08dc5170e592f593d
ajbater <amanda22@hotmail.co.uk>
2017-01-09 14:44:40 +0000

    minor change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var articleList = new ArticleList();
 var controller = new Controller(articleList);
-controller.articleList.createArticle("headline", "full text");
-controller.articleList.createArticle("headline", "full text");
+controller.articleList.createArticle("headline", "summary");
+controller.articleList.createArticle("headline", "summary");
 controller.updateHtml();
 controller.makeHeadlineLinkShowFullArticle();
